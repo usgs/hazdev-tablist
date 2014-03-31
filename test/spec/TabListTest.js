0a42f5c6b5efc2f3911d69ea24d1d5636b573c97
Jeremy Fee <jmfee@usgs.gov>
2014-03-31 09:40:30 -0600

    Added tests

--- test/spec/TabListTest.js
+++ test/spec/TabListTest.js
@@ -2,9 +2,11 @@
 
 define([
 	'chai',
+	'sinon',
 	'tablist/TabList'
 ], function (
 	chai,
+	sinon,
 	TabList
 ) {
 	'use strict';
@@ -14,7 +16,48 @@ define([
 
 	describe('Unit tests for the "TabList" class', function () {
 
+		describe('constructor()', function () {
+
+			it('only loads content for selected tab', function () {
+				var tl = new TabList({
+					tabs: [
+						{
+							title: 'title1',
+							content: 'content1'
+						},
+						{
+							title: 'title2',
+							content: 'content2',
+							selected: true
+						}
+					]
+				});
+
+				expect(tl._tabs[0].contentReady).to.equal(false);
+				expect(tl._tabs[1].contentReady).to.equal(true);
+			});
+
+		});
+
 		describe('addTab()', function () {
+
+			it('selects the first tab by default', function () {
+				var testEl = document.createElement('div');
+				var tabList = new TabList({
+					el: testEl
+				});
+
+				var tab1Ref = tabList.addTab({
+					title: 'title1',
+					content: 'content1'
+				});
+
+				expect(tab1Ref.contentReady).to.equal(true);
+				expect(tab1Ref.tabEl.classList.contains('tablist-tab-selected')).
+						to.equal(true);
+				expect(tab1Ref.panelEl.classList.contains('tablist-panel-selected')).
+						to.equal(true);
+			});
 
 			it('returned object has select() method that selects tab', function () {
 
@@ -84,6 +127,55 @@ define([
 				// check panel role and aria-labelledby attribute
 				expect(panelEl.getAttribute('role')).to.equal('tabpanel');
 				expect(panelEl.getAttribute('aria-labelledby')).to.equal(tabEl.id);
+			});
+
+		});
+
+		describe('_selectTab()', function () {
+
+			it('loads tab content once, calls onSelect callback each time', function () {
+				var tl,
+				    tabOptions,
+				    contentSpy,
+				    onSelectSpy,
+				    tabRef;
+
+				// create tablist with a tab, which will be selected
+				tl = new TabList({
+					tabs: [
+						{
+							title: 'tab1',
+							content: 'content1'
+						}
+					]
+				});
+				// add a tab with callback spies
+				tabOptions = {
+					title: 'my title',
+					content: function () {
+						return 'my content';
+					},
+					onSelect: function () {}
+				};
+				contentSpy = sinon.spy(tabOptions, 'content');
+				onSelectSpy = sinon.spy(tabOptions, 'onSelect');
+				tabRef = tl.addTab(tabOptions);
+
+				// not selected
+				expect(tabRef.contentReady).to.equal(false);
+				expect(contentSpy.callCount).to.equal(0);
+				expect(onSelectSpy.callCount).to.equal(0);
+
+				// select first time
+				tabRef.select();
+				expect(tabRef.contentReady).to.equal(true);
+				expect(contentSpy.callCount).to.equal(1);
+				expect(onSelectSpy.callCount).to.equal(1);
+
+				// select second time
+				tabRef.select();
+				expect(contentSpy.callCount).to.equal(1);
+				expect(onSelectSpy.callCount).to.equal(2);
 			});
 
 		});

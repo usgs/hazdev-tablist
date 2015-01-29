d6f7ef4052c5be65830f57b30b1000c59efa948d
ehunter-usgs <ehunter@usgs.gov>
2015-01-29 15:32:32 -0700

    Update tablist tests to work with updated code.

--- test/spec/TabListTest.js
+++ test/spec/TabListTest.js
@@ -1,337 +1,286 @@
-/* global define, describe, it, beforeEach, afterEach */
+/* global describe, it, beforeEach, afterEach */
 
-define([
-  'chai',
-  'sinon',
-  'tablist/TabList'
-], function (
-  chai,
-  sinon,
-  TabList
-) {
-  'use strict';
+'use strict';
 
-  var expect = chai.expect;
+var chai = require('chai'),
+    sinon = require('sinon'),
+    TabList = require('tablist/TabList');
 
-  var getClickEvent = function () {
-    var clickEvent = document.createEvent('MouseEvents');
-    clickEvent.initMouseEvent('click', true, true, window, 1, 0, 0);
-    return clickEvent;
-  };
+var expect = chai.expect;
 
-  describe('Unit tests for the "TabList" class', function () {
+var getClickEvent = function () {
+  var clickEvent = document.createEvent('MouseEvents');
+  clickEvent.initMouseEvent('click', true, true, window, 1, 0, 0);
+  return clickEvent;
+};
 
-    describe('constructor()', function () {
+describe('Unit tests for the "TabList" class', function () {
 
-      it('only loads content for selected tab', function () {
-        var tl = new TabList({
-          tabs: [
-            {
-              title: 'title1',
-              content: 'content1'
-            },
-            {
-              title: 'title2',
-              content: 'content2',
-              selected: true
-            }
-          ]
-        });
+  describe('constructor()', function () {
 
-        expect(tl._tabs[0].contentReady).to.equal(false);
-        expect(tl._tabs[1].contentReady).to.equal(true);
-      });
-
-    });
-
-    describe('addTab()', function () {
-
-      it('selects the first tab by default', function () {
-        var testEl = document.createElement('div');
-        var tabList = new TabList({
-          el: testEl
-        });
-
-        var tab1Ref = tabList.addTab({
-          title: 'title1',
-          content: 'content1'
-        });
-
-        expect(tab1Ref.contentReady).to.equal(true);
-        expect(tab1Ref.tabEl.classList.contains('tablist-tab-selected')).
-            to.equal(true);
-        expect(tab1Ref.panelEl.classList.contains('tablist-panel-selected')).
-            to.equal(true);
-      });
-
-      it('returned object has select() method that selects tab', function () {
-
-        var testEl = document.createElement('div');
-        var tabList = new TabList({
-          el: testEl
-        });
-
-        var tab1Ref = tabList.addTab({
-          title: 'title1',
-          content: 'content1'
-        });
-        var tab2Ref = tabList.addTab({
-          title: 'title2',
-          content: 'content2'
-        });
-
-        var tabEl1 = tab1Ref.tabEl.classList,
-            panelEl1 = tab1Ref.panelEl.classList,
-            tabEl2 = tab2Ref.tabEl.classList,
-            panelEl2 = tab2Ref.panelEl.classList,
-            tabSelected = 'tablist-tab-selected',
-            panelSelected = 'tablist-panel-selected';
-
-        // first tab added is selected by default
-        expect(tabEl1.contains(tabSelected)).to.equal(true);
-        expect(panelEl1.contains(panelSelected)).to.equal(true);
-        expect(tabEl2.contains(tabSelected)).to.equal(false);
-        expect(panelEl2.contains(panelSelected)).to.equal(false);
-        // now select tab 2 to confirm classes have changed
-        tab2Ref.select();
-        expect(tabEl1.contains(tabSelected)).to.equal(false);
-        expect(panelEl1.contains(panelSelected)).to.equal(false);
-        expect(tabEl2.contains(tabSelected)).to.equal(true);
-        expect(panelEl2.contains(panelSelected)).to.equal(true);
-        // and select tab 1 again to confirm classes have changed back
-        tab1Ref.select();
-        expect(tabEl1.contains(tabSelected)).to.equal(true);
-        expect(panelEl1.contains(panelSelected)).to.equal(true);
-        expect(tabEl2.contains(tabSelected)).to.equal(false);
-        expect(panelEl2.contains(panelSelected)).to.equal(false);
-
-      });
-
-      it('sets up aria relationships between tab and panel', function () {
-        var testEl = document.createElement('div');
-        var tabList = new TabList({
-          el: testEl
-        });
-
-        // check tablist role
-        var navEl = testEl.querySelector('nav');
-        expect(navEl.getAttribute('role')).to.equal('tablist');
-
-        // add a tab
-        var tab1Ref = tabList.addTab({
-          title: 'title1',
-          content: 'content1'
-        });
-        var tabEl = tab1Ref.tabEl;
-        var panelEl = tab1Ref.panelEl;
-
-        // check tab role and aria-controls attribute
-        expect(tabEl.getAttribute('role')).to.equal('tab');
-        expect(tabEl.getAttribute('aria-controls')).to.equal(panelEl.id);
-
-        // check panel role and aria-labelledby attribute
-        expect(panelEl.getAttribute('role')).to.equal('tabpanel');
-        expect(panelEl.getAttribute('aria-labelledby')).to.equal(tabEl.id);
-      });
-
-    });
-
-    describe('_selectTab()', function () {
-
-      it('loads tab content once, calls onSelect callback each time',
-          function () {
-        var tl,
-            tabOptions,
-            contentSpy,
-            onSelectSpy,
-            tabRef;
-
-        // create tablist with a tab, which will be selected
-        tl = new TabList({
-          tabs: [
-            {
-              title: 'tab1',
-              content: 'content1'
-            }
-          ]
-        });
-        // add a tab with callback spies
-        tabOptions = {
-          title: 'my title',
-          content: function () {
-            return 'my content';
+    it('only loads content for selected tab', function () {
+      var tl = TabList({
+        tabs: [
+          {
+            title: 'title1',
+            content: 'content1'
           },
-          onSelect: function () {}
-        };
-        contentSpy = sinon.spy(tabOptions, 'content');
-        onSelectSpy = sinon.spy(tabOptions, 'onSelect');
-        tabRef = tl.addTab(tabOptions);
-
-        // not selected
-        expect(tabRef.contentReady).to.equal(false);
-        expect(contentSpy.callCount).to.equal(0);
-        expect(onSelectSpy.callCount).to.equal(0);
-
-        // select first time
-        tabRef.select();
-        expect(tabRef.contentReady).to.equal(true);
-        expect(contentSpy.callCount).to.equal(1);
-        expect(onSelectSpy.callCount).to.equal(1);
-
-        // select second time
-        tabRef.select();
-        expect(contentSpy.callCount).to.equal(1);
-        expect(onSelectSpy.callCount).to.equal(2);
+          {
+            title: 'title2',
+            content: 'content2',
+            selected: true
+          }
+        ]
       });
+
+      expect(tl.tabs[0].contentReady).to.equal(false);
+      expect(tl.tabs[1].contentReady).to.equal(true);
+    });
+
+  });
+
+  describe('addTab()', function () {
+
+    it('selects the first tab by default', function () {
+      var testEl = document.createElement('div');
+      var tabList = TabList({
+        el: testEl
+      });
+
+      var tab1Ref = tabList.addTab({
+        title: 'title1',
+        content: 'content1'
+      });
+
+      expect(tab1Ref.contentReady).to.equal(true);
+      expect(tab1Ref.tabEl.classList.contains('tablist-tab-selected')).
+          to.equal(true);
+      expect(tab1Ref.panelEl.classList.contains('tablist-panel-selected')).
+          to.equal(true);
+    });
+
+    it('returned object has select() method that selects tab', function () {
+
+      var testEl = document.createElement('div');
+      var tabList = TabList({
+        el: testEl
+      });
+
+      var tab1Ref = tabList.addTab({
+        title: 'title1',
+        content: 'content1'
+      });
+      var tab2Ref = tabList.addTab({
+        title: 'title2',
+        content: 'content2'
+      });
+
+      var tabEl1 = tab1Ref.tabEl.classList,
+          panelEl1 = tab1Ref.panelEl.classList,
+          tabEl2 = tab2Ref.tabEl.classList,
+          panelEl2 = tab2Ref.panelEl.classList,
+          tabSelected = 'tablist-tab-selected',
+          panelSelected = 'tablist-panel-selected';
+
+      // first tab added is selected by default
+      expect(tabEl1.contains(tabSelected)).to.equal(true);
+      expect(panelEl1.contains(panelSelected)).to.equal(true);
+      expect(tabEl2.contains(tabSelected)).to.equal(false);
+      expect(panelEl2.contains(panelSelected)).to.equal(false);
+      // now select tab 2 to confirm classes have changed
+      tab2Ref.select();
+      expect(tabEl1.contains(tabSelected)).to.equal(false);
+      expect(panelEl1.contains(panelSelected)).to.equal(false);
+      expect(tabEl2.contains(tabSelected)).to.equal(true);
+      expect(panelEl2.contains(panelSelected)).to.equal(true);
+      // and select tab 1 again to confirm classes have changed back
+      tab1Ref.select();
+      expect(tabEl1.contains(tabSelected)).to.equal(true);
+      expect(panelEl1.contains(panelSelected)).to.equal(true);
+      expect(tabEl2.contains(tabSelected)).to.equal(false);
+      expect(panelEl2.contains(panelSelected)).to.equal(false);
 
     });
 
-    describe('event bindings', function () {
-      var tabList,
-          keyPressSpy,
-          forwardButtonSpy,
-          previousButtonSpy,
-          dragStartSpy,
-          dragEndSpy;
-
-      beforeEach(function () {
-        keyPressSpy = sinon.spy(TabList.prototype, '_onKeyPress');
-        forwardButtonSpy = sinon.spy(TabList.prototype, '_selectNextTab');
-        previousButtonSpy = sinon.spy(TabList.prototype, '_selectPreviousTab');
-        dragStartSpy = sinon.spy(TabList.prototype, '_onDragStart');
-        dragEndSpy = sinon.spy(TabList.prototype, '_onDragEnd');
-
-        tabList = new TabList({
-          tabs: [
-            {
-              title: 'tab1',
-              content: 'content1'
-            },
-            {
-              title: 'tab2',
-              content: 'content2'
-            },
-            {
-              title: 'tab3',
-              content: 'content3'
-            }
-          ]
-        });
+    it('sets up aria relationships between tab and panel', function () {
+      var testEl = document.createElement('div');
+      var tabList = new TabList({
+        el: testEl
       });
 
-      afterEach(function () {
-        keyPressSpy.restore();
-        forwardButtonSpy.restore();
-        previousButtonSpy.restore();
-        dragStartSpy.restore();
-        dragEndSpy.restore();
+      // check tablist role
+      var navEl = testEl.querySelector('nav');
+      expect(navEl.getAttribute('role')).to.equal('tablist');
+
+      // add a tab
+      var tab1Ref = tabList.addTab({
+        title: 'title1',
+        content: 'content1'
+      });
+      var tabEl = tab1Ref.tabEl;
+      var panelEl = tab1Ref.panelEl;
+
+      // check tab role and aria-controls attribute
+      expect(tabEl.getAttribute('role')).to.equal('tab');
+      expect(tabEl.getAttribute('aria-controls')).to.equal(panelEl.id);
+
+      // check panel role and aria-labelledby attribute
+      expect(panelEl.getAttribute('role')).to.equal('tabpanel');
+      expect(panelEl.getAttribute('aria-labelledby')).to.equal(tabEl.id);
+    });
+
+  });
+
+  describe('_selectTab()', function () {
+
+    it('loads tab content once, calls onSelect callback each time',
+        function () {
+      var tl,
+          tabOptions,
+          contentSpy,
+          onSelectSpy,
+          tabRef;
+
+      // create tablist with a tab, which will be selected
+      tl = new TabList({
+        tabs: [
+          {
+            title: 'tab1',
+            content: 'content1'
+          }
+        ]
+      });
+      // add a tab with callback spies
+      tabOptions = {
+        title: 'my title',
+        content: function () {
+          return 'my content';
+        },
+        onSelect: function () {}
+      };
+      contentSpy = sinon.spy(tabOptions, 'content');
+      onSelectSpy = sinon.spy(tabOptions, 'onSelect');
+      tabRef = tl.addTab(tabOptions);
+
+      // not selected
+      expect(tabRef.contentReady).to.equal(false);
+      expect(contentSpy.callCount).to.equal(0);
+      expect(onSelectSpy.callCount).to.equal(0);
+
+      // select first time
+      tabRef.select();
+      expect(tabRef.contentReady).to.equal(true);
+      expect(contentSpy.callCount).to.equal(1);
+      expect(onSelectSpy.callCount).to.equal(1);
+
+      // select second time
+      tabRef.select();
+      expect(contentSpy.callCount).to.equal(1);
+      expect(onSelectSpy.callCount).to.equal(2);
+    });
+
+  });
+
+  describe('navigate using the tablist navigation', function () {
+    var tabList,
+        tabs,
+        nav,
+        selected,
+        before,
+        after,
+        forwardButton,
+        backwardButton,
+        getSelectedTab;
+
+    beforeEach(function () {
+
+      tabList = TabList({
+        tabs: [
+          {
+            title: 'tab1',
+            content: 'content1'
+          },
+          {
+            title: 'tab2',
+            content: 'content2'
+          },
+          {
+            title: 'tab3',
+            content: 'content3'
+          }
+        ]
       });
 
-      it('responds to a click event on tablist navigation buttons',
-          function () {
-        tabList._forward.dispatchEvent(getClickEvent());
-        tabList._backward.dispatchEvent(getClickEvent());
-        expect(forwardButtonSpy.callCount).to.equal(1);
-        expect(previousButtonSpy.callCount).to.equal(1);
-      });
+      nav = tabList.el.querySelector('nav');
+      forwardButton = tabList.el.querySelector('.tablist-forward-button');
+      backwardButton = tabList.el.querySelector('.tablist-backward-button');
+      tabs = tabList.el.querySelectorAll('.tablist-tab');
 
-      it('responds to a mousedown event on tablist navigation', function () {
-        var mousedownEvent = document.createEvent('MouseEvents');
-        mousedownEvent.initMouseEvent('mousedown', true, true, window, 1, 0, 0);
-
-        tabList._nav.dispatchEvent(mousedownEvent);
-        expect(dragStartSpy.callCount).to.equal(1);
-      });
-
-      it('responds to a mouseup event on tablist navigation', function () {
-        var mousedownEvent = document.createEvent('MouseEvents');
-        mousedownEvent.initMouseEvent('mousedown', true, true, window, 1, 0, 0);
-        var mouseupEvent = document.createEvent('MouseEvents');
-        mouseupEvent.initMouseEvent('mouseup', true, true, window, 1, 0, 0);
-
-        // mouseup is added to body, tablist needs to be added to document...
-        var body = document.querySelector('body');
-        body.appendChild(tabList._el);
-
-        tabList._nav.dispatchEvent(mousedownEvent);
-        tabList._nav.dispatchEvent(mouseupEvent);
-
-        // detach now that events are done.
-        body.removeChild(tabList._el);
-        expect(dragEndSpy.callCount).to.equal(1);
-      });
-
-      it('responds to a mouseleave event on tablist navigation', function () {
-        var mouseleaveEvent = document.createEvent('MouseEvents'),
-            mousedownEvent = document.createEvent('MouseEvents');
-
-        mousedownEvent.initMouseEvent('mousedown', true, true,
-            window, 1, 0, 0);
-        mouseleaveEvent.initMouseEvent('mouseleave', true, true,
-            window, 1, 0, 0);
-
-        tabList._nav.dispatchEvent(mousedownEvent);
-        tabList._nav.dispatchEvent(mouseleaveEvent);
-        expect(dragStartSpy.callCount).to.equal(1);
-      });
+      getSelectedTab = function () {
+        for (var i = 0; i < tabs.length; i++) {
+          if (tabs[i].classList.contains('tablist-tab-selected')) {
+            selected = tabs[i];
+          }
+        }
+        return selected;
+      };
 
     });
 
-    describe('navigating using tablist nav', function () {
-      var tabList;
+    afterEach(function () {
+      tabList = null;
+      tabs = null;
+      selected = null;
+      before = null;
+      after = null;
+    });
 
-      beforeEach(function () {
-        tabList = new TabList({
-          tabs: [
-            {
-              title: 'tab1',
-              content: 'content1'
-            },
-            {
-              title: 'tab2',
-              content: 'content2'
-            },
-            {
-              title: 'tab3',
-              content: 'content3'
-            }
-          ]
-        });
+    it('selects the next tab when the forward button is clicked',
+        function () {
 
-      });
+      before = getSelectedTab();
+      forwardButton.dispatchEvent(getClickEvent());
+      after = getSelectedTab();
 
-      afterEach(function() {
-        tabList = null;
-      });
+      expect(before.innerHTML).to.equal('tab1');
+      expect(before.innerHTML).to.not.equal(after.innerHTML);
+      expect(after.innerHTML).to.equal('tab2');
+    });
 
-      it('click on a tab to load content', function () {
-        var tab4Ref, tabEl, panelEl;
+    it('selects the previous tab when the backward button is clicked',
+        function () {
 
-        tab4Ref = tabList.addTab({
-          title: 'tab4',
-          content: 'content4'
-        });
+      before = getSelectedTab();
+      backwardButton.dispatchEvent(getClickEvent());
+      after = getSelectedTab();
 
-        tabEl = tab4Ref.tabEl;
-        panelEl = tab4Ref.panelEl;
+      expect(before.innerHTML).to.equal('tab1');
+      expect(before.innerHTML).to.not.equal(after.innerHTML);
+      expect(after.innerHTML).to.equal('tab3');
+    });
 
-        tabEl.dispatchEvent(getClickEvent());
-        expect(tabList._selected).to.be.equal(tab4Ref);
-      });
+    it('selects the correct tab when the tab is clicked',
+        function () {
 
-      it('use forward and backward button to navigate tabs', function () {
-        var forwardButton, backwardButton;
+      before = getSelectedTab();
+      tabs[1].dispatchEvent(getClickEvent());
+      after = getSelectedTab();
 
-        forwardButton = tabList._forward;
-        backwardButton = tabList._backward;
+      expect(before.innerHTML).to.equal('tab1');
+      expect(before.innerHTML).to.not.equal(after.innerHTML);
+      expect(after.innerHTML).to.equal('tab2');
+    });
 
-        forwardButton.dispatchEvent(getClickEvent());
-        expect(tabList._selected.tabEl.innerHTML).to.be.equal('tab2');
 
-        backwardButton.dispatchEvent(getClickEvent());
-        expect(tabList._selected.tabEl.innerHTML).to.be.equal('tab1');
-      });
+    it('does nothing when a tab is clicked for the second time',
+        function () {
 
+      before = getSelectedTab();
+      before.dispatchEvent(getClickEvent());
+      after = getSelectedTab();
+
+      expect(before.innerHTML).to.equal('tab1');
+      expect(before).to.equal(after);
     });
   });
 
